221fbd8040b70efb646d755a10955ac737843ef6
Revku <office@revku.dev>
2022-10-28 13:25:49 +0200

    Update footer logo url

--- src/components/organisms/Footer/Footer.js
+++ src/components/organisms/Footer/Footer.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import styles from './Footer.module.scss'
-import revkuLogo from 'assets/revku-logo.svg'
+import styles from "./Footer.module.scss";
+import revkuLogo from "assets/revku-logo.svg";
 
 const Footer = () => {
   return (
     <div className={styles.wrapper}>
-        <p className={styles.paragraph}>
-            Powered by
-
-            <a href="https://revku.pl/" target="_blank" rel="noopener noreferrer" className={styles.link}>
-                <img src={revkuLogo} alt="Revku Logo" className={styles.logo} />
-            </a>
-        </p>
+      <p className={styles.paragraph}>
+        Powered by
+        <a
+          href="https://revku.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+        >
+          <img src={revkuLogo} alt="Revku Logo" className={styles.logo} />
+        </a>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
